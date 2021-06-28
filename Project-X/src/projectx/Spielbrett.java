4a66c136d7e00e543d40468d11dd5b102a7e6759
Seraph141 <janik.irlenbusch@edu.bib.de>
2021-06-28 09:23:41 +0200

    spielbrett + zelle

---
+++ Project-X/src/projectx/Spielbrett.java
@@ -0,0 +1,20 @@
+package projectx;
+
+public class Spielbrett {
+
+	private Zelle[][] spielfeld;
+
+	public Spielbrett() {
+
+	}
+
+	public Zelle[][] getSpielfeld() {
+		return spielfeld;
+	}
+
+	public void setSpielfeld(Zelle[][] spielfeld) {
+		this.spielfeld = spielfeld;
+	}
+
+
+}
---
+++ Project-X/src/projectx/Zelle.java
@@ -0,0 +1,32 @@
+package projectx;
+
+public class Zelle {
+
+	private boolean isSpezialFeld;
+	private static int temp = 0;
+	private int id;
+	
+	public Zelle(int id, boolean isSpezialFeld) {
+		
+		
+	}
+
+	public boolean isSpezialFeld() {
+		return isSpezialFeld;
+	}
+
+	public void setSpezialFeld(boolean isSpezialFeld) {
+		this.isSpezialFeld = isSpezialFeld;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+}
