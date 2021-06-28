1eda0a9d41238c393c385974d8b52b79c0f6e82e
DaTiMy <37105868+DaTiMy@users.noreply.github.com>
2021-06-28 10:02:04 +0200

    Spielfeld

--- Project-X/src/projectx/Spielbrett.java
+++ Project-X/src/projectx/Spielbrett.java
@@ -12,36 +12,36 @@ public class Spielbrett {
 		return spielfeld;
 	}
 
-	public void setSpielfeld(Zelle[] spielfeld) {
-		this.spielfeld = spielfeld;
+	public void setSpielfeld() {
+		
 		for(int i = 0; i <= 50; i++)
 		{
-			spielfeld[i] = new Zelle(false);
+			this.spielfeld[i] = new Zelle(false);
 			switch(i)
 			{
 				case 6:
-					spielfeld[i] = new Leiter(14,true);
+					this.spielfeld[i] = new Leiter(14,true);
 					break;
 				case 16:
-					spielfeld[i] = new Schlange(3,true);
+					this.spielfeld[i] = new Schlange(3,true);
 					break;
 				case 17:
-					spielfeld[i] = new Leiter(23,true);
+					this.spielfeld[i] = new Leiter(23,true);
 					break;
 				case 27:
-					spielfeld[i] = new Leiter(33,true);
+					this.spielfeld[i] = new Leiter(33,true);
 					break;
 				case 29:
-					spielfeld[i] = new Schlange(10,true);
+					this.spielfeld[i] = new Schlange(10,true);
 					break;
 				case 38:
-					spielfeld[i] = new Leiter(43, true);
+					this.spielfeld[i] = new Leiter(43, true);
 					break;
 				case 39:
-					spielfeld[i] = new Schlange(20, true);
+					this.spielfeld[i] = new Schlange(20, true);
 					break;
 				case 45:
-					spielfeld[i] = new Schlange(34,true);
+					this.spielfeld[i] = new Schlange(34,true);
 					break;
 			}
 		}
